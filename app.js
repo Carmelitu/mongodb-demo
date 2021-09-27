4e58dcba43a29e837d73799e45db2634ad7d34c7
Lucas Seijo <lucaseijo7@gmail.com>
2021-09-26 21:18:46 -0300

    guardando curso en DB

--- app.js
+++ app.js
@@ -1,5 +1,30 @@
 const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
 mongoose.connect('mongodb://localhost:27017/test')
     .then( () => console.log('Conectado a MongoDB'))
     .catch( error => console.log(error));
+
+const cursoSchema =  new Schema({
+    nombre: String,
+    autor: String,
+    etiquetas: [String],
+    fecha: {type: Date, default: Date.now},
+    publicado: Boolean
+});
+
+const Curso = mongoose.model('Curso', cursoSchema);
+
+async function crearCurso(){
+    const curso = new Curso({
+        nombre: 'Node.JS',
+        autor: 'Seiji',
+        etiquetas: ['js', 'frontend'],
+        publicado: true
+    });
+    
+    const resultado = await curso.save();
+    console.log(resultado);
+}
+
+crearCurso();
